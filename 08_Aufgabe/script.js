e7e859496a4a545ef1db59e7d3fd6aaf8d678788
Jan-schweizer-99 <jan.schweizer@hs-furtwangen.de>
2021-05-16 23:25:01 +0200

    Aufgabe 8
    mit einer nicht funktionierender
    Zusatzaufgabe

--- 08_Aufgabe/script.js
+++ 08_Aufgabe/script.js
@@ -1,28 +1,102 @@
-var aufgabe_7;
-(function (aufgabe_7) {
+var aufgabe_8;
+(function (aufgabe_8) {
+    var translated;
+    var myVar;
+    var myVar1;
+    var counter = 0;
+    var aufnahme = false;
+    var index = 0;
+    var sound1 = 1;
+    var sound2 = 2;
+    var sound3 = 3;
+    var sound4 = 4;
+    var sound5 = 5;
+    var sound6 = 6;
+    var sound7 = 7;
+    var sound8 = 8;
+    var sound9 = 9;
+    var playarray = [sound1, sound2, sound3, sound4, sound5, sound6, sound7, sound8, sound9];
+    var eigenerSound = false;
     window.addEventListener("load", function () {
+        //Toggle random and play
+        console.log(aufnahme);
+        document.getElementById("trashBin").addEventListener("click", function () {
+            document.getElementById("playButton").classList.add("is-hidden");
+            document.getElementById("pauseButton").classList.remove("is-hidden");
+            document.getElementById("mic").classList.add("is-hidden");
+            document.getElementById("micDeactivated").classList.remove("is-hidden");
+            document.getElementById("randomZ").classList.remove("is-hidden");
+            document.getElementById("randomPlay").classList.add("is-hidden");
+            clearInterval(myVar);
+            clearInterval(myVar1);
+            eigenerSound = false;
+            aufnahme = false;
+            console.log(eigenerSound);
+        });
+        document.getElementById("playButton").addEventListener("click", function () {
+            this.classList.add("is-hidden");
+            document.getElementById("pauseButton").classList.remove("is-hidden");
+            clearInterval(myVar1);
+            eigenerSound = false;
+            console.log(eigenerSound);
+        });
+        document.getElementById("pauseButton").addEventListener("click", function () {
+            this.classList.add("is-hidden");
+            document.getElementById("playButton").classList.remove("is-hidden");
+            myVar1 = setInterval(mysound, 500);
+            eigenerSound = true;
+            console.log(eigenerSound);
+        });
+        document.getElementById("randomZ").addEventListener("click", function () {
+            this.classList.add("is-hidden");
+            document.getElementById("randomPlay").classList.remove("is-hidden");
+            myVar = setInterval(random, 500);
+        });
+        document.getElementById("randomPlay").addEventListener("click", function () {
+            this.classList.add("is-hidden");
+            document.getElementById("randomZ").classList.remove("is-hidden");
+            clearInterval(myVar);
+        });
+        document.getElementById("mic").addEventListener("click", function () {
+            this.classList.add("is-hidden");
+            document.getElementById("micDeactivated").classList.remove("is-hidden");
+            aufnahme = false;
+            console.log(aufnahme);
+        });
+        document.getElementById("micDeactivated").addEventListener("click", function () {
+            this.classList.add("is-hidden");
+            document.getElementById("mic").classList.remove("is-hidden");
+            aufnahme = true;
+            console.log(aufnahme);
+        });
+        var array = [];
         var _loop_1 = function (num) {
             document.querySelector("#pad" + num).addEventListener("mousedown", function () { playSample("assets/sound_" + num + ".mp3"); }); //Init mousedown events
+            document.querySelector("#pad" + num).addEventListener("mousedown", function () { if (aufnahme == true && counter <= 8) {
+                countup(counter++, num);
+            }
+            else {
+                counter = 0;
+                countup(counter++, num);
+            } });
         };
         for (var num = 1; num <= 9; num++) {
             _loop_1(num);
-        }
-        document.querySelector("#play3Samples").addEventListener("mousedown", function () { play3Samples(); }); //Init mousedown event playbutten
-        //document.querySelector("#pad1").addEventListener("mousedown", function(){playSample("assets/sound_1.mp3");}); //Block der mit  init for Schleife vereinfacht wurde
-        // document.querySelector("#pad2").addEventListener("mousedown", function(){playSample("assets/sound_2.mp3");});
-        // document.querySelector("#pad3").addEventListener("mousedown", function(){playSample("assets/sound_3.mp3");});
-        // document.querySelector("#pad4").addEventListener("mousedown", function(){playSample("assets/sound_4.mp3");});
-        // document.querySelector("#pad5").addEventListener("mousedown", function(){playSample("assets/sound_5.mp3");});
-        // document.querySelector("#pad6").addEventListener("mousedown", function(){playSample("assets/sound_6.mp3");});
-        // document.querySelector("#pad7").addEventListener("mousedown", function(){playSample("assets/sound_7.mp3");});
-        // document.querySelector("#pad8").addEventListener("mousedown", function(){playSample("assets/sound_8.mp3");});
-        // document.querySelector("#pad9").addEventListener("mousedown", function(){playSample("assets/sound_9.mp3");});
+        } //Init mousedown events
         var translate = [7, 8, 9, 4, 5, 6, 1, 2, 3];
         var add = 96;
         var _loop_2 = function (num) {
-            document.addEventListener('keydown', function (event) {
+            document.addEventListener("keydown", function (event) {
                 if (event.keyCode == add + num) {
+                    translated = translate[num - 1];
                     playSample("assets/sound_" + translate[num - 1] + ".mp3");
+                    if (aufnahme == true && counter <= 8) {
+                        countup(counter++, translated);
+                        countup(counter++, num);
+                    }
+                    else {
+                        counter = 0;
+                    }
                 }
             });
         };
@@ -30,23 +104,78 @@ var aufgabe_7;
             _loop_2(num);
         }
     });
-    var x = document.getElementById("pad1").innerText;
     function playSample(samples) {
         var sound = new Audio(samples);
         sound.play();
     }
-    ;
-    function play3Samples() {
-        var delay = 0;
-        var _loop_3 = function (sound) {
-            setTimeout(function () { playSample("assets/sound_" + sound + ".mp3"); }, delay);
-            delay = delay + 500;
-            console.log("assets/sound_" + sound + ".mp3");
-        };
-        for (var sound = 7; sound <= 9; sound++) {
-            _loop_3(sound);
+    function countup(counter, translated) {
+        var _a;
+        if (aufnahme == true) {
+            console.log(counter);
+            console.log(translated);
+            console.log((_a = [translated], counter = _a[0], _a));
+            for (var index = 0; index < 1; index++) {
+                //playarray[index] = sound1;
+                //sound1 = translated;
+                switch (counter) {
+                    case 0:
+                        sound1 = translated;
+                        console.log(sound1);
+                        break;
+                    case 1:
+                        sound2 = translated;
+                        console.log(sound2);
+                        break;
+                    case 2:
+                        sound3 = translated;
+                        console.log(sound3);
+                        break;
+                    case 3:
+                        sound4 = translated;
+                        console.log(sound4);
+                        break;
+                    case 4:
+                        sound5 = translated;
+                        console.log(sound5);
+                        break;
+                    case 5:
+                        sound6 = translated;
+                        console.log(sound6);
+                        break;
+                    case 6:
+                        sound7 = translated;
+                        console.log(sound7);
+                        break;
+                    case 7:
+                        sound8 = translated;
+                        console.log(sound8);
+                        break;
+                    case 8:
+                        sound9 = translated;
+                        console.log(sound9);
+                        break;
+                }
+            }
+            //let array: number [] = [translated];
         }
     }
-    ;
-})(aufgabe_7 || (aufgabe_7 = {}));
+    function random() {
+        playSample("assets/sound_" + (Math.floor(Math.random() * 9) + 1) + ".mp3");
+    }
+    function mysound() {
+        if (eigenerSound == true) {
+            playSample("assets/sound_" + playarray[index++] + ".mp3");
+            if (index == 9) {
+                index = 0;
+            }
+        }
+    }
+    // function random(): void { //play 3 random samples
+    //     var delay: number = 0;
+    //     for (let index: number = 0; index < 1; index++) {
+    //         setTimeout(function (): void { playSample("assets/sound_" + (Math.floor(Math.random() * 9) + 1) + ".mp3"); }, delay);
+    //         delay += 1000;
+    //     }
+    // }
+})(aufgabe_8 || (aufgabe_8 = {}));
 //# sourceMappingURL=script.js.map
--- 08_Aufgabe/script.ts
+++ 08_Aufgabe/script.ts
@@ -1,44 +1,167 @@
-namespace aufgabe_7 {
+namespace aufgabe_8 {
+    var translated: number;
+    var myVar: number;
+    var myVar1: number;
+    let counter: number = 0;
+    var aufnahme: boolean = false;
+    let index: number = 0;
+    let sound1: number = 1;
+    let sound2: number = 2;
+    let sound3: number = 3;
+    let sound4: number = 4;
+    let sound5: number = 5;
+    let sound6: number = 6;
+    let sound7: number = 7;
+    let sound8: number = 8;
+    let sound9: number = 9;
+    var playarray: number [] = [sound1, sound2, sound3, sound4, sound5, sound6, sound7, sound8, sound9];
+    var eigenerSound: boolean = false;
+    window.addEventListener("load", function (): void { //Init mousedown events
+        //Toggle random and play
+        console.log(aufnahme);
+        document.getElementById("trashBin").addEventListener("click", function (): void {
+            document.getElementById("playButton").classList.add("is-hidden");
+            document.getElementById("pauseButton").classList.remove("is-hidden");
+            document.getElementById("mic").classList.add("is-hidden");
+            document.getElementById("micDeactivated").classList.remove("is-hidden");
+            document.getElementById("randomZ").classList.remove("is-hidden");
+            document.getElementById("randomPlay").classList.add("is-hidden");
+            clearInterval(myVar);
+            clearInterval(myVar1);
+            eigenerSound = false;
+            aufnahme = false;
+            console.log(eigenerSound);
+        });
+        document.getElementById("playButton").addEventListener("click", function (): void {
+            this.classList.add("is-hidden");
+            document.getElementById("pauseButton").classList.remove("is-hidden");
+            clearInterval(myVar1);
+            eigenerSound = false;
+            console.log(eigenerSound);
+        });
 
-    window.addEventListener("load", function () { //Init mousedown events
-        for (let num = 1; num <= 9; num++) { //Init mousedown events
-            document.querySelector("#pad" + num).addEventListener("mousedown", function () { playSample("assets/sound_" + num + ".mp3"); }); //Init mousedown events
-        }
-        document.querySelector("#play3Samples").addEventListener("mousedown", function () { play3Samples(); });   //Init mousedown event playbutten
+        document.getElementById("pauseButton").addEventListener("click", function (): void {
+            this.classList.add("is-hidden");
+            document.getElementById("playButton").classList.remove("is-hidden");
+            myVar1 = setInterval(mysound, 500);
+            eigenerSound = true;
+            console.log(eigenerSound);
+        });
+        document.getElementById("randomZ").addEventListener("click", function (): void {
+            this.classList.add("is-hidden");
+            document.getElementById("randomPlay").classList.remove("is-hidden");
+            myVar = setInterval(random, 500);
+        });
 
-        //document.querySelector("#pad1").addEventListener("mousedown", function(){playSample("assets/sound_1.mp3");}); //Block der mit  init for Schleife vereinfacht wurde
-        // document.querySelector("#pad2").addEventListener("mousedown", function(){playSample("assets/sound_2.mp3");});
-        // document.querySelector("#pad3").addEventListener("mousedown", function(){playSample("assets/sound_3.mp3");});
-        // document.querySelector("#pad4").addEventListener("mousedown", function(){playSample("assets/sound_4.mp3");});
-        // document.querySelector("#pad5").addEventListener("mousedown", function(){playSample("assets/sound_5.mp3");});
-        // document.querySelector("#pad6").addEventListener("mousedown", function(){playSample("assets/sound_6.mp3");});
-        // document.querySelector("#pad7").addEventListener("mousedown", function(){playSample("assets/sound_7.mp3");});
-        // document.querySelector("#pad8").addEventListener("mousedown", function(){playSample("assets/sound_8.mp3");});
-        // document.querySelector("#pad9").addEventListener("mousedown", function(){playSample("assets/sound_9.mp3");});
+        document.getElementById("randomPlay").addEventListener("click", function (): void {
+            this.classList.add("is-hidden");
+            document.getElementById("randomZ").classList.remove("is-hidden");
+            clearInterval(myVar);
+        });
+        document.getElementById("mic").addEventListener("click", function (): void {
+            this.classList.add("is-hidden");
+            document.getElementById("micDeactivated").classList.remove("is-hidden");
+            aufnahme = false;
+            console.log(aufnahme);
+        });
+        document.getElementById("micDeactivated").addEventListener("click", function (): void {
+            this.classList.add("is-hidden");
+            document.getElementById("mic").classList.remove("is-hidden");
+            aufnahme = true;
+            console.log(aufnahme);
+
+        });
+
+        let array: number[] = [];
+        for (let num: number = 1; num <= 9; num++) { //Init mousedown events
+            document.querySelector("#pad" + num).addEventListener("mousedown", function (): void { playSample("assets/sound_" + num + ".mp3"); }); //Init mousedown events
+
+            document.querySelector("#pad" + num).addEventListener("mousedown", function (): void { if (aufnahme == true && counter <= 8) { countup(counter++, num); } else { counter = 0; countup(counter++, num); } });
+        } //Init mousedown events
+
         let translate: number[] = [7, 8, 9, 4, 5, 6, 1, 2, 3];
         var add: number = 96;
-        for (let num:number = 1; num <= 9; num++) { //ubersetzung auf Tastatur
-            document.addEventListener('keydown', function (event:KeyboardEvent) { //ubersetzung auf Tastatur
+        for (let num: number = 1; num <= 9; num++) { //ubersetzung auf Tastatur
+            document.addEventListener("keydown", function (event: KeyboardEvent): void { //ubersetzung auf Tastatur
                 if (event.keyCode == add + num) {
+                    translated = translate[num - 1];
                     playSample("assets/sound_" + translate[num - 1] + ".mp3");
+                    if (aufnahme == true && counter <= 8) { countup(counter++, translated); countup(counter++, num); }
+                    else { counter = 0; }
                 }
             });
         }
     });
-
-    var x = document.getElementById("pad1").innerText;
-
     function playSample(samples: string): void { //play Sample funktion
         var sound: HTMLAudioElement = new Audio(samples);
         sound.play();
-    };
-    function play3Samples() {  //play 3 Samples funktion
-        var delay: number = 0;
-        for (let sound = 7; sound <= 9; sound++) {
-            setTimeout(function () { playSample("assets/sound_" + sound + ".mp3"); }, delay);
-            delay = delay + 500;
-            console.log("assets/sound_" + sound + ".mp3")
+    }
+    function countup(counter: number, translated: number): void {
+ if (aufnahme == true) {
+        console.log(counter);
+        console.log(translated);
+        console.log([counter] = [translated]);
+        for (var index: number = 0; index < 1; index++) {
+            //playarray[index] = sound1;
+            //sound1 = translated;
+            switch (counter) {
+                case 0:
+                    sound1 = translated;
+                    console.log(sound1);
+                    break;
+                case 1:
+                    sound2 = translated;
+                    console.log(sound2);
+                    break;
+                case 2:
+                    sound3 = translated;
+                    console.log(sound3);
+                    break;
+                case 3:
+                    sound4 = translated;
+                    console.log(sound4);
+                    break;
+                case 4:
+                    sound5 = translated;
+                    console.log(sound5);
+                    break;
+                case 5:
+                    sound6 = translated;
+                    console.log(sound6);
+                    break;
+                case 6:
+                    sound7 = translated;
+                    console.log(sound7);
+                    break;
+                case 7:
+                    sound8 = translated;
+                    console.log(sound8);
+                    break;
+                case 8:
+                    sound9 = translated;
+                    console.log(sound9);
+                    break;
+                    }
         }
-    };
+        //let array: number [] = [translated];
+    }
+}  
 
+    function random(): void { //play my samples
+        playSample("assets/sound_" + (Math.floor(Math.random() * 9) + 1) + ".mp3");
+    }
+    function mysound(): void {
+        if (eigenerSound == true) {
+            playSample("assets/sound_" + playarray[index++] + ".mp3");
+            if (index == 9) {index = 0; }
+        }
+    }
+    // function random(): void { //play 3 random samples
+    //     var delay: number = 0;
+    //     for (let index: number = 0; index < 1; index++) {
+    //         setTimeout(function (): void { playSample("assets/sound_" + (Math.floor(Math.random() * 9) + 1) + ".mp3"); }, delay);
+    //         delay += 1000;
+    //     }
+
+    // }
 }
